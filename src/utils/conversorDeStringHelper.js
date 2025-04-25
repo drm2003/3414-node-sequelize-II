728b1ffc2939acdd46adc4838f9131bafd6d005c
Daniel R. Monteiro <daniel.monteiro@tjdft.jus.br>
2025-04-24 21:40:22 -0300

    incluindo novo arquivo de helper

---
+++ src/utils/conversorDeStringHelper.js
@@ -0,0 +1,10 @@
+module.exports = (objetoParams) => {
+  for(let propriedade in objetoParams) {
+    if(/Id|id/.test(propriedade)){
+      objetoParams[propriedade] = Number(objetoParams[propriedade]);
+    }
+  }
+
+
+  return objetoParams;
+};
